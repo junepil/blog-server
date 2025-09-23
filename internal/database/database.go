0f1c3d5aae4264dc58650b4a1757e86a209914be
Junepil Lee <hanu9257@gmail.com>
2025-09-23 16:38:43 +0800

    Refactor DB connection configuration

--- internal/database/database.go
+++ internal/database/database.go
@@ -13,13 +13,14 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s",
-		os.Getenv("DB_HOST"),
+	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
+		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
 	)
+
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
